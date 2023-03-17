51a078c478b1858131a915c97b47eb3528041ec6
Abhay7898 <128139505+Abhay7898@users.noreply.github.com>
2023-03-17 17:56:38 +0530

    nwe

--- Mayur_Gorakhpuria_Study/src/collectiontest/ArrayListTest.java
+++ Mayur_Gorakhpuria_Study/src/collectiontest/ArrayListTest.java
@@ -1,21 +1,46 @@
 package collectiontest;
 
 public class ArrayListTest {
+	import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		System.out.println("Mayur");
-		System.out.println();
-		System.out.println("Abhay");
-		System.out.println("Garhwal");
-		System.out.println("nandini");
-		System.out.println("nandini1");
+import arrayListTestNew.Coustmer;
+import arrayListTestNew.Employee;
+import arrayListTestNew.Student;
+import arrayListTestNew.a;
+
+	public class TestNew {
+
+	public static void main(String []args) {
+ArrayList a = new ArrayList<>();            ArrayList a1 = new ArrayList<>();    
+		Employee e = new Employee();           	  Employee e1 = new Employee();
+		e.setId(01);												  e1.setId(02);
+		Coustmer c = new Coustmer();              Coustmer c1 = new Coustmer();        
+		c.setName("Abhay");                                c1.setName("Abhay");
+		Student  s = new Student ();                    Student  s1 = new Student ();
+		s.setPost("Worker");                                   s1.setPost("Worker");
 		
-		for (int i = 0; i < args.length; i++) {
-			System.out.println(i);
-			
-			System.out.println("mayur");
-			System.out.println("Mayur g");
-			System.out.println("abhay gadhwal");
-		}
+		/*a.add(e);         a1.add(e1);      a.addAll(a1);
+		a.add(s);         a1.add(s1);       
+		a.add(c);         a1.add(c1);*/
+		a.add(e);
+
+		//System.out.println(e.getId());        
+		//System.out.println(e1.getId());
+		
+		//System.out.println(e.toString());
+		//e1.setId(02);
+		System.out.println(a.contains(s1)); 
+		
+		//a.add(0, e1); 
+		a.set(0, e1);
+		//System.out.println(a.contains(e));
+		System.out.println(e.getId());
+		System.out.println(a.contains(e1)); // mujhe replace value ko print karvana he 
+		System.out.println(a.set(0, e1));
+		System.out.println(a.);
+		
+		
 	}
+	}
+
 }
