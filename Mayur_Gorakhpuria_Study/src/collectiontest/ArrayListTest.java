a0a413ad7517b56425d601f58995532b4649df1a
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-18 16:43:41 +0530

    Update ArrayListTest.java

--- Mayur_Gorakhpuria_Study/src/collectiontest/ArrayListTest.java
+++ Mayur_Gorakhpuria_Study/src/collectiontest/ArrayListTest.java
@@ -1,8 +1,29 @@
 package collectiontest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayListTest {
 
 	public static void main(String[] args) {
+		
+	//	ArrayList<String> list = new ArrayList<>();;
+		
+		
+		String str = "The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.";
+		
+		String word[] = str.split("\\s");
+		
+		/*for (int i = 0; i < word.length; i++) {
+			
+			//list.add(word[i]);
+			
+		}*/
+		ArrayList<String> list = new ArrayList<>(Arrays.asList(word));
+
+		
+		System.out.println(list);
+		
 
 	}
 
