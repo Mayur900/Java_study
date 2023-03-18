b65d45f148595b26c750566bd8693b089900310c
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-18 18:34:15 +0530

    zx

---
+++ Mayur_Gorakhpuria_Study/src/collectiontest/HashSetTest.java
@@ -0,0 +1,39 @@
+package collectiontest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class HashSetTest {
+	public static void main(String[] args) {
+		
+		ArrayList<String> list2 = new ArrayList<>(Arrays.asList("dtu"));
+
+		
+		HashSet<String> hs = new HashSet<>();
+		
+		hs.add("abc");
+		
+		hs.add("abc");
+		
+		hs.add("xyz");
+		
+		
+		//hs.clear();
+		
+		
+		HashSet<String> hs1 = new HashSet<>();
+		hs1.add("who");
+		
+		
+		hs.addAll(hs1);
+		hs.addAll(list2);
+		
+//		hs.forEach(s -> System.out.print(s + " "));
+		
+		
+		//System.out.println(hs);
+		
+	}
+
+}
---
+++ Mayur_Gorakhpuria_Study/src/string_test/StringTest.java
@@ -0,0 +1,50 @@
+package string_test;
+
+public class StringTest {
+	public static void main(String[] args) {
+
+
+		String str = "The Collection in Java is a framework c provides an architecture to store and manipulate the group of objects.";
+
+		String s = "abc";
+
+		String s1 = new String("abc");
+
+		String s2 = new String("abc");
+		
+		String s3 = s2;
+
+		
+//		System.out.println(s == s1);
+//		System.out.println(s1 == s2);
+		
+//		System.out.println(s2 == s3);
+		
+//		System.out.println(s.equals(s1));
+		
+//		System.out.println(s1.equals(s2));
+		
+//		System.out.println(s2.equals(s3));
+		
+
+//		System.out.println(s.charAt(2));
+		
+//		System.out.println(s);
+
+//		s = s.concat("xyx");
+		
+//		System.out.println(s);
+		
+//		System.out.println(str.contains("itec"));
+		
+		
+//		System.out.println(Arrays.toString(s.getBytes()));
+		
+//		System.out.println(str.substring(14));
+		
+//		System.out.println(str.substring(6, 13));
+		
+			
+	}
+
+}
