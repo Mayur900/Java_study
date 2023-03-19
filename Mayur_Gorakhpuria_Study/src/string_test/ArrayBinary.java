e136edbaf6c3eeecd1cfadcf38678ae30f6d20d7
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-19 17:30:23 +0530

    Update ArrayBinary.java

--- Mayur_Gorakhpuria_Study/src/string_test/ArrayBinary.java
+++ Mayur_Gorakhpuria_Study/src/string_test/ArrayBinary.java
@@ -1,10 +1,29 @@
 package string_test;
 
+import java.util.Arrays;
+
 public class ArrayBinary {
 
 	public static void main(String[] args) {
-		
-    int arr[]={1,0,0,0,1,1,0,1,0,1,1,1,0,1};
 
-	}	
+		int arr[] = { 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] == 1) {
+				arr[i] = 0;
+			} else if (arr[i] == 0) {
+				arr[i] = 1;
+			}
+
+		}
+
+		// for print array
+		for (int j : arr) {
+			System.out.print(j + " ");
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(arr));
+
+	}
 }
