9bb3769e458e8cc43d34c07025a711911750affb
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-19 18:03:12 +0530

    Update ReversLastWordOfString.java

--- Mayur_Gorakhpuria_Study/src/logicalProgram/ReversLastWordOfString.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/ReversLastWordOfString.java
@@ -1,10 +1,10 @@
 package logicalProgram;
 
 public class ReversLastWordOfString {
+	public static void main(String[] args) {
 
-	String str = "The Collection in Java is a framework";
-				//The Collection in Java is a krowemarf
-												
-	
+		String str = "The Collection in Java is a framework";
+		// The Collection in Java is a krowemarf
 
+	}
 }
