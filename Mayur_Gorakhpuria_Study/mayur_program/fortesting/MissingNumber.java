9500afc311bb8d81118a6ea4f335895139a86ab4
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-19 17:53:04 +0530

    sd

--- Mayur_Gorakhpuria_Study/mayur_program/fortesting/MissingNumber.java
+++ Mayur_Gorakhpuria_Study/mayur_program/fortesting/MissingNumber.java
@@ -1,29 +1,24 @@
 package fortesting;
 
-public class MissingNumber
-{
-    
-    public static void main(String[] args) {
-        
-        int[] n = {1,2,3,4,5,7,8,9,10};  
-       int l = 0;
-        
-        for (int j = 0; j < n.length; j++) {
-            
-             l += n[j];
-            
-        }
-        System.out.println(l);
-        
-    int  total  = (n.length+1)*(n.length+2)/2; 
-        System.out.println(total);
-    for (int  i = 0; i< n.length; i++)
-    {
-       total -= n[i];
-    }
-   
-        System.out.println(total);
-    }
-    
-    
+public class MissingNumber {
+
+	public static void main(String[] args) {
+
+		int[] n = { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+		int l = 0;
+
+		for (int j = 0; j < n.length; j++) {
+
+			l += n[j];
+
+		}
+
+		int total = (n.length + 1) * (n.length + 2) / 2;
+		for (int i = 0; i < n.length; i++) {
+			total -= n[i];
+		}
+
+		System.out.println(total);
+	}
+
 }
--- Mayur_Gorakhpuria_Study/src/logicalProgram/MissingNumber.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/MissingNumber.java
@@ -4,6 +4,16 @@ public class MissingNumber {
 	public static void main(String[] args) {
 		
 		int[] n = { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
+		
+		int missingNumber = n[0];
+		
+		for(int num : n) {
+			if(missingNumber != num) {
+				System.out.println(missingNumber);
+				break;
+			}
+			missingNumber++;
+		}
 
 	}
 }
