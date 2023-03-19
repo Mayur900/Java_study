af007f6786f56265ef83dfbb41d5aa282a1f8158
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-19 18:02:51 +0530

    Create ReversLastWordOfString.java

---
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/ReversLastWordOfString.java
@@ -0,0 +1,10 @@
+package logicalProgram;
+
+public class ReversLastWordOfString {
+
+	String str = "The Collection in Java is a framework";
+				//The Collection in Java is a krowemarf
+												
+	
+
+}
