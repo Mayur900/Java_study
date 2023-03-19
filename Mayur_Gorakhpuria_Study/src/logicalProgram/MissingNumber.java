2e1397d4f77d9bc7157b80f37fd7944921b7c3ff
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-19 18:00:30 +0530

    Update MissingNumber.java

--- Mayur_Gorakhpuria_Study/src/logicalProgram/MissingNumber.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/MissingNumber.java
@@ -2,18 +2,18 @@ package logicalProgram;
 
 public class MissingNumber {
 	public static void main(String[] args) {
-		
+
 		int[] n = { 1, 2, 3, 4, 5, 7, 8, 9, 10 };
-		
+
 		int missingNumber = n[0];
-		
-		for(int num : n) {
-			if(missingNumber != num) {
+
+		for (int i = 0; i < n.length; i++) {
+
+			if (missingNumber != n[i]) {
 				System.out.println(missingNumber);
 				break;
 			}
 			missingNumber++;
 		}
-
 	}
 }
