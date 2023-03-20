7279fba2572019a1c46c947ed51a332d4c35ae80
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-20 16:14:44 +0530

    sdsd

---
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/Remve_Vowel_From_String.java
@@ -0,0 +1,7 @@
+package logicalProgram;
+
+public class Remve_Vowel_From_String {
+	public static void main(String[] args) {
+		String s = "It uses those low order bytes of the result that can fit into the size of the type allowed by the operation";
+	}
+}
--- Mayur_Gorakhpuria_Study/src/logicalProgram/StringNextWord.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/StringNextWord.java
@@ -1,11 +1,18 @@
 package logicalProgram;
 
 public class StringNextWord {
-public static void main(String[] args) {
-	
-    String s = "ABCD";
-    		  //BCDE
+	public static void main(String[] args) {
 
-	
-}
+		String s = "ABHAY";
+		// BCDE
+
+		char ch[] = s.toCharArray();
+
+		for (int i = 0; i < ch.length; i++) {
+			int charWord = ch[i] + 1;
+			System.out.print((char) charWord);
+
+		}
+
+	}
 }
