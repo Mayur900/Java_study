19e6f6ab3f5d4dcac17a840f8f43d871bd7cc5fa
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-20 16:57:17 +0530

    Update Remove_Vowel_From_String.java

--- Mayur_Gorakhpuria_Study/src/logicalProgram/Remove_Vowel_From_String.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/Remove_Vowel_From_String.java
@@ -1,7 +1,57 @@
 package logicalProgram;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Remove_Vowel_From_String {
 	public static void main(String[] args) {
 		String s = "It uses those low order bytes of the result that can fit into the size of the type allowed by the operation";
+
+		char ch[] = s.toCharArray();
+
+		/*
+		 * for (char c : ch) { if (c != 'a' && c != 'i' && c != 'o' && c != 'u' && c !=
+		 * 'e') { System.out.print(c); } }
+		 */
+		
+		/**
+		char vowel[] = {'a', 'e', 'i', 'o', 'u'};
+		for (int i = 0; i < ch.length; i++) {
+			char c = ch[i];
+			for(char ce : vowel) {
+				if(c == ce) {
+					ch[i] = 'Z';
+				}
+			}
+		}
+		for(char sw : ch){
+			if(sw != 'Z') {
+				System.out.print(sw);
+			}
+		}
+		**/
+		ArrayList<Character> list = new ArrayList<>();
+		for(char cs : ch) {
+			list.add(cs);
+		}
+		
+		char vowel[] = {'a', 'e', 'i', 'o', 'u'};
+		ArrayList<Character> list1 = new ArrayList<>();
+		for(char cs : vowel) {
+			list1.add(cs);
+		}
+		
+		list.removeAll(list1);
+		
+		for(Character c : list) {
+			System.out.print(c);
+		}
+
+
+		
+		
+
 	}
 }
