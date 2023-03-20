badb3fe41b20fd3cc87c89eaf4df3e6dd87efc59
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-20 16:17:04 +0530

    s

--- Mayur_Gorakhpuria_Study/src/logicalProgram/Remove_Vowel_From_String.java
+++ Mayur_Gorakhpuria_Study/src/logicalProgram/Remove_Vowel_From_String.java
@@ -1,6 +1,6 @@
 package logicalProgram;
 
-public class Remve_Vowel_From_String {
+public class Remove_Vowel_From_String {
 	public static void main(String[] args) {
 		String s = "It uses those low order bytes of the result that can fit into the size of the type allowed by the operation";
 	}
