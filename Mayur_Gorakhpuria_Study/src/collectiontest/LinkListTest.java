817141e0f133a7be401a9e5bb4a40f13d1b1887c
Mayur900 <128124045+Mayur900@users.noreply.github.com>
2023-03-22 15:13:27 +0530

    ss

---
+++ Mayur_Gorakhpuria_Study/src/collectiontest/LinkListTest.java
@@ -0,0 +1,16 @@
+package collectiontest;
+
+import java.util.LinkedList;
+
+public class LinkListTest {
+	
+	public static void main(String[] args) {
+		
+	LinkedList<String> llist  =new LinkedList<>();
+	llist.add("String");
+	llist.add("hello");
+	
+	System.out.println(llist);
+	
+	}
+}
---
+++ Mayur_Gorakhpuria_Study/src/collectiontest/VectorTest.java
@@ -0,0 +1,37 @@
+package collectiontest;
+
+import java.util.Vector;
+
+public class VectorTest {
+
+	public static void main(String[] args) {
+
+		Vector<String> vl = new Vector<>();
+		vl.add("hello");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		vl.add("sleep");
+		
+		vl.add("sleep");
+		
+		System.out.println(vl);
+	}
+
+}
